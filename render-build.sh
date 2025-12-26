77f9dab981f2b054fa0c4be12a9668a6183aa148
danipg9 <danixdpg9@gmail.com>
2025-12-26 19:41:33 +0100

    [fix]render conf

--- render-build.sh
+++ render-build.sh
@@ -4,7 +4,3 @@ set -o errexit
 
 # 1. Instalar dependencias
 pip install -r requirements.txt
-
-# 2. Crear tablas en la base de datos remota (Postgres)
-# Ejecutamos el script que creamos antes para inicializar la BD
-python init_db.py
